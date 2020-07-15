beb6a2cdf2de69f488a3b7ac937dff52d9df62ed
Michael Pedersen Starmer <michael@mstarmer.no>
2020-07-15 18:06:50 +0200

    Finds vims colors dir automatically

--- customizers/vim-install-monokai.sh
+++ customizers/vim-install-monokai.sh
@@ -7,7 +7,7 @@ COLOR_DIR=$(echo "$(ls -d /usr/share/vim/vim*/)" | awk '{print $1}')
 
 echo "Color path: $COLOR_DIR"
 
-printf "[*] Downloading color scheme" && { curl -o ${COLOR_DIR}colors/monokai.vim https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim ; }
+curl -o ${COLOR_DIR}colors/monokai.vim https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim 2>/dev/null || exit 1
 
 printf "\nWriting to .vimrc in users home dir: ${USER_HOME}\n"
 cd ${USER_HOME} &&
